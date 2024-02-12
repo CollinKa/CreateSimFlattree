51beab7ad56703e0965595ab332c3b53b6733511
CollinKa <Colin.ka9@gmail.com>
2024-02-11 20:32:19 -0800

    fix without photon run number issue

--- flatlight.py
+++ flatlight.py
@@ -139,10 +139,10 @@ def create_branches_event(output_tree, event, runNumber):
     output_tree.Branch("event", event, "event/I")
     output_tree.Branch("runNumber", runNumber, "runNumber/I")
 
-def populate_vectors_event(input_tree, event, runNumber):
+def populate_vectors_event(input_tree, event, runNumber,fileNumber):
     evt = input_tree.ROOTEvent
     event[0] = evt.GetEventID()
-    runNumber[0] = 1
+    runNumber[0] = fileNumber
 
 ##################################################################################################################
 # Main script
@@ -154,6 +154,9 @@ if ROOT.gSystem.Load("/net/cms26/cms26r0/zheng/barSimulation/WithPhotonUpdateSim
 #filename = "/net/cms26/cms26r0/zheng/barSimulation/barWithPhotonUpdate/BARcosmic" + sys.argv[1] + "/MilliQan.root"
 filename = sys.argv[1] + sys.argv[2] + "/MilliQan.root"
 outname = "output_" + sys.argv[2] + ".root"
+
+#find the file number
+fileNumber = int(sys.argv[2])
 
 input_file = ROOT.TFile(filename, "READ")
 
@@ -198,7 +201,7 @@ for i in range(n_entries):
     input_tree.GetEntry(i)
 
     # Populate the vectors with flattened data
-    populate_vectors_event(input_tree, event, runNumber)
+    populate_vectors_event(input_tree, event, runNumber,fileNumber)
     populate_vectors_scint(input_tree, scint_copyNo, scint_layer, scint_nPE, scint_time)
     
     # Fill the new tree with the flattened data
